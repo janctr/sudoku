5621feee6fcde89831c818112508eaded08ed43d
janctr <janiverson.eligio.ctr@us.navy.mil>
2022-12-14 14:22:20 -1000

    Move old test puzzles to different file.

--- src/sudokuController.ts
+++ src/sudokuController.ts
@@ -1,30 +1,6 @@
 import { SudokuPuzzle, SudokuPuzzleBasic } from "./types";
 import { makepuzzle, solvepuzzle, ratepuzzle } from "sudoku";
 
-const puzzleA = [
-  [2, 3, 0, 4, 1, 5, 0, 6, 8],
-  [0, 8, 0, 2, 3, 6, 5, 1, 9],
-  [1, 6, 0, 9, 8, 7, 2, 3, 4],
-  [3, 1, 7, 0, 9, 4, 0, 2, 5],
-  [4, 5, 8, 1, 2, 0, 6, 9, 7],
-  [9, 2, 6, 0, 5, 8, 3, 0, 1],
-  [0, 0, 0, 5, 0, 0, 1, 0, 2],
-  [0, 0, 0, 8, 4, 2, 9, 0, 3],
-  [5, 9, 2, 3, 7, 1, 4, 8, 6],
-];
-
-const puzzleAAnswer = [
-  [2, 3, 9, 4, 1, 5, 7, 6, 8],
-  [7, 8, 4, 2, 3, 6, 5, 1, 9],
-  [1, 6, 5, 9, 8, 7, 2, 3, 4],
-  [3, 1, 7, 6, 9, 4, 8, 2, 5],
-  [4, 5, 8, 1, 2, 3, 6, 9, 7],
-  [9, 2, 6, 7, 5, 8, 3, 4, 1],
-  [8, 4, 3, 5, 6, 9, 1, 7, 2],
-  [6, 7, 1, 8, 4, 2, 9, 5, 3],
-  [5, 9, 2, 3, 7, 1, 4, 8, 6],
-];
-
 function convertSudokuPuzzle(regularPuzzle: number[]): SudokuPuzzle {
   const convertedPuzzle = initEmptyGrid();
 
